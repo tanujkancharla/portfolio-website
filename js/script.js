4e2fcbcf29b4b79557593abc0ac21b9b275a370b
tanujisnowheretobeseen <141784626+tanujisnowheretobeseen@users.noreply.github.com>
2026-07-04 23:56:02 -0700

    Add DOM manipulation for portfolio heading

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ console.log("Portfolio website loaded successfully.");
 let studentName = "Tanuj";
 
 console.log(studentName);
+
+let university = "CSU East Bay";
+let major = "Computer Science";
+
+console.log(studentName);
+console.log(university);
+console.log(major);
+
+const title = document.getElementById("title");
+console.log(title);
+title.textContent = "Tanuj Kancharla 🚀";
